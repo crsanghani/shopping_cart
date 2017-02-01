83451b03d3c2290ee1df267647499b0a2a81a5f7
Chetan Sanghani <crsanghani@gmail.com>
2017-02-01 23:29:20 +0000

    README.md: updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # shopping_cart
+
+Develop a functional shopping cart e-commerce site with as much of the below functionality as possible in the available time.it is not necessary for the solution to process payments.
+
+## Functionality
+
+- Product list and details should be retrieved via api to get them from a database
+- It should be possible to both rate products and add comments about the products
+- Comments should be restricted to a maximum of 350 characters
+- view multiple products and add them to the shopping cart
+- Add remove items from the shopping cart and change the quantity of items in the shopping cart
+- The shopping cart should display and calculate individual item prices and a total price of the items in the cart. The state of the shopping cart should also keep its stat if the browser is closed and reopened
--- client/components/cart_list.js
+++ client/components/cart_list.js
@@ -1,55 +1,55 @@
-// import React, { Component } from 'react';
-//
-// class Cart extends Component {
-//   constructor(props){
-//     super(props);
-//
-//     this.state = { items: [], total: 0 };
-//   }
-//
-//   addItem(e, item) {
-//     this.state.items.push(index);
-//     this.countTotal();
-//   }
-//
-//   removeItem(e, itemID) {
-//     let itemIndex;
-//     this.state.items.some(function(item,index){
-//       if(item.id == itemId){
-//         itemIndex = index;
-//         return true;
-//         this.countTotal();
-//       }
-//     })
-//
-//     if(typeof itemID != 'undefined'){
-//       this.state.items.spice(itemIndex, 1);
-//     }
-//   }
-//
-//   countTotal() {
-//     let total = 0;
-//     this.state.items.forEach(function(item, index){
-//       total = total + item.price;
-//     });
-//     this.setState(total: total);
-//   }
-//
-//   render()
-//     return(
-//       <div className='cart-list'>
-//         {this.state.items.map(item =>
-//           return(
-//             <li key={item.id} className='list-group-item'>
-//               <p>{item.name}</p>
-//               <p>{item.price}</p>
-//             </li>
-//           )
-//         )};
-//         let body = (<ul>{items}</ul>)
-//         let empty = <div>cart is empty</div>;
-//     </div>
-//   );
-// };
-//
-// export default Cart;
+import React, { Component } from 'react';
+
+class CartList extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { items: [], total: 0 };
+  }
+
+  addItem(e, item) {
+    this.state.items.push(index);
+    this.countTotal();
+  }
+
+  removeItem(e, itemID) {
+    let itemIndex;
+    this.state.items.some(function(item,index){
+      if(item.id == itemId){
+        itemIndex = index;
+        return true;
+        this.countTotal();
+      }
+    })
+
+    if(typeof itemID != 'undefined'){
+      this.state.items.spice(itemIndex, 1);
+    }
+  }
+
+  countTotal() {
+    let total = 0;
+    this.state.items.forEach(function(item, index){
+      total = total + item.price;
+    });
+    this.setState(total: total);
+  }
+
+  render()
+    return(
+      <div className='cart-list'>
+        {this.state.items.map(item =>
+          return(
+            <li key={item.id} className='list-group-item'>
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+            </li>
+          )
+        )};
+        let body = (<ul>{items}</ul>)
+        let empty = <div>cart is empty</div>;
+    </div>
+  );
+};
+
+export default Cart;
